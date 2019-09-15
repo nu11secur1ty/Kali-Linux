81d5c25635696a1cc0e079c89aec6685fce9b4ce
nu11secur1ty <venvaropt@gmail.com>
2019-09-15 22:01:22 +0300

    Update bk.sh

--- BlueKeep/bk.sh
+++ BlueKeep/bk.sh
@@ -13,7 +13,7 @@ cd /usr/share/metasploit-framework/modules/exploits/windows
                 cd $HOME
         msfdb init
         echo -e "\e[1;31mCopy and paste this command into your Metasploit session: (use exploit/windows/rdp/cve_2019_0708_bluekeep_rce)\e[0m"
-            wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/BlueKeep/msfcall.sh 
+            wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/BlueKeep/msfcall
      chmod a+x msfcall
      gnome-terminal -e ./msfcall
      sleep 3;
