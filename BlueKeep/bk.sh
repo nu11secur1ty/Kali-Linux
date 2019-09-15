180328062a539f243ddc804742d3416bc846a4fd
nu11secur1ty <venvaropt@gmail.com>
2019-09-15 18:15:19 +0300

    Create bk.sh

---
+++ BlueKeep/bk.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+cd /opt
+git clone https://github.com/rapid7/metasploit-framework.git
+cd metasploit-framework
+git fetch origin pull/12283/head:bluekeep
+git checkout bluekeep
+gem install bundler && bundle
