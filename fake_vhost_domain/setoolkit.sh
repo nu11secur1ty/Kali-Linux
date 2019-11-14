003439f6c74e7753af0672ef61218a2ce1bf3598
nu11secur1ty <venvaropt@gmail.com>
2019-11-14 22:37:05 +0200

    Update setoolkit.sh

--- fake_vhost_domain/setoolkit.sh
+++ fake_vhost_domain/setoolkit.sh
@@ -3,37 +3,13 @@
 #
 #
 # Prepare apache
-cd /etc/apache2/sites-available 
-mv 000-default.conf 000-default.conf.backup
-
-cd /root/.set/web_clone
-
-# Creating vhost
-cd /etc/apache2/sites-available/
-	cat > facebook.com.conf << EOF
-<VirtualHost *:80>
-    ServerName facebook.com
-    ServerAlias www.facebook.com
-    DocumentRoot /root/.set/web_clone
-
-    <Directory /root/.set/web_clone>
-        Options -Indexes +FollowSymLinks
-        AllowOverride All
-    </Directory>
-
-    ErrorLog ${APACHE_LOG_DIR}/$fuck-error.log
-    CustomLog ${APACHE_LOG_DIR}/$fuck-access.log combined
-</VirtualHost>
-EOF
-# Enabling vhost
-a2ensite facebook.com
-systemctl restart apache2
-
 cd /etc/
 	echo "Type you IP"
 	read IP
+	echo "Type your fake domain"
+	read fuck
 		cat >> hosts << EOF
-$IP facebook.com
+$IP $fuck
 EOF
 	systemctl restart apache2
 	exit 0;
