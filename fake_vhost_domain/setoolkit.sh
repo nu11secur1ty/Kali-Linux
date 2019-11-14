998e0271a75b4198aa4168b4cecf6d107e521511
nu11secur1ty <venvaropt@gmail.com>
2019-11-14 20:46:31 +0200

    Update setoolkit.sh

--- fake_vhost_domain/setoolkit.sh
+++ fake_vhost_domain/setoolkit.sh
@@ -6,50 +6,18 @@
 cd /etc/apache2/sites-available 
 mv 000-default.conf 000-default.conf.backup
 
-echo "Type your fake domain, for example: 'kurec.com'"
-read fuck
-
-mkdir -p /var/www/$fuck/public_html
-cd /var/www/$fuck/public_html/
-	cat > index.html << EOF
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Welcome to your fucking production</title>
-  </head>
-  <body>
-    <h1>Success! Welcome to your fucking production $fck set up completed!</h1>
-  </body>
-</html>
-EOF
 # Creating vhost
-chown -R www-data: /var/www/$fuck
 cd /etc/apache2/sites-available/
-	cat > $fuck.conf << EOF
+	cat > setoolkit.conf << EOF
 <VirtualHost *:80>
-    ServerName $fuck.com
-    ServerAlias www.$fuck
-    DocumentRoot /var/www/$fuck/public_html
-
-    <Directory /var/www/$fuck/public_html>
+    DocumentRoot /root/.set/web_clone
+<Directory /root/.set/web_clone>
         Options -Indexes +FollowSymLinks
         AllowOverride All
     </Directory>
-
     ErrorLog ${APACHE_LOG_DIR}/$fuck-error.log
     CustomLog ${APACHE_LOG_DIR}/$fuck-access.log combined
 </VirtualHost>
 EOF
-# Enabling vhost
-a2ensite $fuck
-systemctl restart apache2
-
-cd /etc/
-	echo "Type you IP"
-	read IP
-		cat >> hosts << EOF
-$IP $fuck
-EOF
-	systemctl restart apache2
+systemct start apache2
 	exit 0;
