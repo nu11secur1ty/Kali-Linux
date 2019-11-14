f3394ad1fd59a1f1c5dbc9e51473270db09ded1d
nu11secur1ty <venvaropt@gmail.com>
2019-11-14 22:49:32 +0200

    Update remove_setoolkit.sh

--- fake_vhost_domain/remove_setoolkit.sh
+++ fake_vhost_domain/remove_setoolkit.sh
@@ -4,6 +4,5 @@
 # Remove fake domain
 	echo "Check the fake domain on which line is"
 	cat -n /etc/hosts
-    echo "Execute the command in your terminal to remove the fake domain it depend on which line numer is\
-    for example sed -i '5d' /etc/hosts"
+    echo "Execute the command: sed -i '5d' /etc/hosts in your terminal to remove the fake domain it depends on which line number is"
 	exit 0;
