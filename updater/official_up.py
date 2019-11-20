2dc92f4397450a793aab11340b1265a8a59020d2
nu11secur1ty <venvaropt@gmail.com>
2019-11-20 11:49:40 +0200

    Create official_up.py

---
+++ updater/official_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# Author @nu11secur1ty
+import os
+os.system('apt update -y')
+os.system('apt upgrade -y')
+os.system('apt autoremove -y')
+os.system('dpkg --configure -a')
+os.system('apt --fix-broken install -y')
