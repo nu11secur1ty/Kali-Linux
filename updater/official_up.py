563ed82833b5c35b628394170fa89029d75befc8
nu11secur1ty <venvaropt@gmail.com>
2019-11-20 12:44:00 +0200

    Update official_up.py

--- updater/official_up.py
+++ updater/official_up.py
@@ -2,7 +2,6 @@
 # Author @nu11secur1ty
 import os
 os.system('apt update -y')
-os.system('apt upgrade -y')
 os.system('apt autoremove -y')
 os.system('dpkg --configure -a')
 os.system('apt --fix-broken install -y')
