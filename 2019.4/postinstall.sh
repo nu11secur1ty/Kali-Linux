9c1c4d60dd45f1d58ee493f74bcf789b8627f8ae
nu11secur1ty <venvaropt@gmail.com>
2019-11-27 13:09:41 -0500

    Update postinstall.sh

--- 2019.4/postinstall.sh
+++ 2019.4/postinstall.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 # Author nu11secut1ty
 
-# removing the fucking keyring
+# Update
 curl -s https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/updater/official_up.py | python
 
 # nu11secur1ty conky monitoring
-curl -s https://raw.githubusercontent.com/nu11secur1ty/conky/master/conky.sh | bash
+# curl -s https://raw.githubusercontent.com/nu11secur1ty/conky/master/conky.sh | bash
+
+# ettercap 0.8.3
+curl -s https://raw.githubusercontent.com/nu11secur1ty/ettercap/master/last_installer.sh | bash
+
 exit 0;
