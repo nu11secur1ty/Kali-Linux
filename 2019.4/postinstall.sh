cd19417e69731ef4dc57b02f4ca680ea098f9fd4
nu11secur1ty <venvaropt@gmail.com>
2019-11-27 16:16:52 +0200

    Update postinstall.sh

--- 2019.4/postinstall.sh
+++ 2019.4/postinstall.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 # remove bullshits
+
+# removing the fucking keyring
 apt purge gnome-keyring
+
+# nu11secur1ty conky monitoring
 curl -s https://raw.githubusercontent.com/nu11secur1ty/conky/master/conky.sh | bash
 exit 0;
