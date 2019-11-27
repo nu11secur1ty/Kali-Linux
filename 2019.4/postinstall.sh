1133ca7131d6f7687b5a7b7b7a9382f37bbfb616
nu11secur1ty <venvaropt@gmail.com>
2019-11-27 14:21:57 -0500

    Update postinstall.sh

--- 2019.4/postinstall.sh
+++ 2019.4/postinstall.sh
@@ -10,4 +10,8 @@ curl -s https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/updater
 # ettercap 0.8.3
 curl -s https://raw.githubusercontent.com/nu11secur1ty/ettercap/master/last_installer.sh | bash
 
+# Cisco VPN
+curl -s https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/VPN-Cisco/vpn.sh | bash
+
+
 exit 0;
