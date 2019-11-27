6ea0ae2a400e9bb55cbaa7ae3c56190b22d0d469
nu11secur1ty <venvaropt@gmail.com>
2019-11-27 14:25:16 -0500

    Update vpn.sh

--- VPN-Cisco/vpn.sh
+++ VPN-Cisco/vpn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-#apt install -y network-manager-openvpn-gnome
-#apt install -y network-manager-vpnc-gnome
+apt install -y network-manager-openvpn-gnome
+apt install -y network-manager-vpnc-gnome
 apt install -y network-manager-openconnect-gnome
 exit 0;
