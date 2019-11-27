77b36507e66e782902129eb81869134de3f219be
nu11secur1ty <venvaropt@gmail.com>
2019-11-27 17:38:43 +0200

    Update postinstall.sh

--- 2019.4/postinstall.sh
+++ 2019.4/postinstall.sh
@@ -3,7 +3,6 @@
 
 # removing the fucking keyring
 curl -s https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/updater/official_up.py | python
-apt purge gnome-keyring
 
 # nu11secur1ty conky monitoring
 curl -s https://raw.githubusercontent.com/nu11secur1ty/conky/master/conky.sh | bash
