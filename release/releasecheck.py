715b37d463af307734b75bf7f5641870a084c396
nu11secur1ty <venvaropt@gmail.com>
2019-12-09 10:25:39 +0200

    Create releasecheck.py

---
+++ release/releasecheck.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from bs4 import BeautifulSoup
+import requests
+import os, os.path, csv
+
+listingurl = "https://cdimage.kali.org/"
+response = requests.get(listingurl)
+release = BeautifulSoup(response.text, "html.parser")
+
+print(release)
