67ae127daad54edb01c282d6648cbdac1bd8ef79
nu11secur1ty <venvaropt@gmail.com>
2019-12-09 11:21:36 +0200

    Update releasecheck.py

--- release/releasecheck.py
+++ release/releasecheck.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from bs4 import BeautifulSoup
 import requests
-import os, os.path, csv
+# import os, os.path, csv
 
 listingurl = "https://cdimage.kali.org/"
 response = requests.get(listingurl)
