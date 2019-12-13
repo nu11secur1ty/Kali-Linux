5b4715c5680c02f19eff70409def5ac75510adfe
nu11secur1ty <venvaropt@gmail.com>
2019-12-13 16:33:47 +0200

    Update installer.sh

--- ettercap/installer.sh
+++ ettercap/installer.sh
@@ -1,1 +1,37 @@
-suoon...:)
+The easiest way:
+
+$ mkdir build
+$ cd build
+$ cmake ../
+$ make
+$ make install
+
+If the build fails because you're missing a dependency:
+
+$ (Install any missing dependencies.)
+$ make clean-all
+$ cmake ../
+$ make
+$ make install
+
+
+#### Bundled libraries
+Ettercap now bundles the following libraries with the source distribution:
+  libnet 1.1.6
+  curl 7.44.0
+  luajit 2.0.4
+  check 0.10.0
+
+
+We will build bundled libraries that Ettercap depends upon, so you 
+don't have to! 
+
+By default, the build system will search for system-provided libraries. If it 
+doesn't find the particular library it wants, it will build the library, itself.
+
+# To disable the use of bundled libraries:
+  $ cmake -DBUNDLED_LIBS=Off ../
+
+# To disable the searching for system-provided libraries, and use 
+# bundled libraries exclusively:
+  $ cmake -DSYSTEM_LIBS=Off ../
