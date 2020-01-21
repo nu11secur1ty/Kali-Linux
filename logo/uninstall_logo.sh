541ed8ce0af0f5ced6b33e9f706c2e1bef1f06ab
nu11secur1ty <venvaropt@gmail.com>
2020-01-21 04:41:11 -0500

    Update uninstall_logo.sh

--- logo/uninstall_logo.sh
+++ logo/uninstall_logo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # author @nu11secur1ty 2020
-sed 's/figlet.*//' .bashrc > uninstall_logo && cat uninstall_logo >> .bashrc && rm uninstall_logo
+sed 's/figlet.*//' .bashrc > uninstall_logo && cat /dev/null > .bashrc && cat uninstall_logo >> .bashrc && rm uninstall_logo
   exit 0;
