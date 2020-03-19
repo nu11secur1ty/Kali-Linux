83c75e80c8ffbfca906b674bfd2d96d226ace10a
nu11secur1ty <venvaropt@gmail.com>
2020-03-19 08:35:29 -0400

    Update viewer.sh

--- TeamViewer/viewer.sh
+++ TeamViewer/viewer.sh
@@ -3,7 +3,7 @@ cd /root/
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 apt install ./teamviewer_amd64.deb -y
 sleep 5
-rm -rf *teamviewer*
+rm -rf *teamviewer_amd64.deb*
 sleep 5;
   exit 0;
 
