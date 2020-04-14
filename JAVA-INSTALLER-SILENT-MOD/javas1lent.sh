cd3e1e805f1921d865c3c93114dc9be9b052efed
nu11secur1ty <venvaropt@gmail.com>
2020-04-14 11:41:39 +0300

    Create javas1lent.sh

---
+++ JAVA-INSTALLER-SILENT-MOD/javas1lent.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# @nu11secur1ty
+
+apt install -y default-jre
+apt install -y default-jdk
+_SILENT_JAVA_OPTIONS="$_JAVA_OPTIONS"
+sleep 1
+unset _JAVA_OPTIONS
+sleep 1
+alias='java "$_SILENT_JAVA_OPTIONS"'
+        echo "Check Version..."
+        sleep 3
+        java -version
+        exit 0;
