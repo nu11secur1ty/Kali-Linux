8fa34dfc8fe27498d52278d3b3841cc49fc952ab
nu11secur1ty <venvaropt@gmail.com>
2020-04-19 12:51:20 +0300

    Update unofficial_up.py

--- updater/unofficial_up.py
+++ updater/unofficial_up.py
@@ -2,15 +2,23 @@
 # Author @nu11secur1ty
 import os
 os.system('apt update -y')
-os.system('apt upgrade -y')
 os.system('apt install -f -y')
 os.system('apt --fix-broken install -y')
-os.system('dpkg --configure -a')
+os.system('apt --fix-missing install -y')
 os.system('apt autoremove -y')
-#
+# Dist upgrade
 os.system('apt dist-upgrade -y')
-os.system('apt install -f -y')
-os.system('apt --fix-broken install -y')
+os.system('apt -y full-upgrade -y')
 os.system('dpkg --configure -a')
+os.system('apt --fix-broken install -y')
+os.system('apt install -f -y')
 os.system('apt autoremove -y')
+
+question = input("If you neet to relogin or reboot? y/n: ")
+start_over = "action"
+if question == "y":
+    start_over = os.system("telinit 6")
+    print(start_over)
+else:
+    raise SystemExit
 
