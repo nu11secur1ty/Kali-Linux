5fc493a9d3a4a10ebdd3cdee7fb097bc8723335a
nu11secur1ty <venvaropt@gmail.com>
2020-04-27 12:54:28 +0300

    Update online.sh

--- updater/online.sh
+++ updater/online.sh
@@ -2,5 +2,7 @@
 sudo rm -rf *unofficial_up.py*
   sleep 3
 wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/updater/unofficial_up.py
+echo "\n";
 sudo python3 unofficial_up.py
+  echo "\n";
   exit 0
