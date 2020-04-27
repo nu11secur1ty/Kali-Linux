73e80c39c8a4d5ee378ab856295e1ca939276069
nu11secur1ty <venvaropt@gmail.com>
2020-04-27 12:12:41 +0300

    Update unofficial_up.py

--- updater/unofficial_up.py
+++ updater/unofficial_up.py
@@ -15,10 +15,8 @@ os.system('sudo apt install -f -y')
 os.system('sudo apt autoremove -y')
 
 question = input("If you need to re-login or reboot? Default is (N)y/n:")
-# start_over = "action"
 if question == "y":
     start_over = os.system("telinit 6")
-    # system(start_over)
 else:
     raise SystemExit
 
