5ced9e11a520a4d74199301d5d0df5cc3ab215e9
nu11secur1ty <venvaropt@gmail.com>
2020-04-27 12:45:42 +0300

    Update online.py

--- updater/online.py
+++ updater/online.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import os
+os.system('sudo rm -rf *unofficial_up.py*')
 os.system('wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/updater/unofficial_up.py')
 os.system('python3 unofficial_up.py')
-os,system('sudo rm -rf *unofficial_up.py*')
