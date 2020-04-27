1d01a13234315480472c92e179fbc015dbb6b8f8
nu11secur1ty <venvaropt@gmail.com>
2020-04-27 11:57:15 +0300

    Update unofficial_up.py

--- updater/unofficial_up.py
+++ updater/unofficial_up.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 # Author @nu11secur1ty
 import os
-os.system('apt update -y')
-os.system('apt install -f -y')
-os.system('apt --fix-broken install -y')
-os.system('apt --fix-missing install -y')
-os.system('apt autoremove -y')
+os.system('sudo apt update -y')
+os.system('sudo apt install -f -y')
+os.system('sudo apt --fix-broken install -y')
+os.system('sudo apt --fix-missing install -y')
+os.system('sudo apt autoremove -y')
 # Dist upgrade
-os.system('apt dist-upgrade -y')
-os.system('apt -y full-upgrade -y')
-os.system('dpkg --configure -a')
-os.system('apt --fix-broken install -y')
-os.system('apt install -f -y')
-os.system('apt autoremove -y')
+os.system('sudo apt dist-upgrade -y')
+os.system('sudo apt -y full-upgrade -y')
+os.system('sudo dpkg --configure -a')
+os.system('sudo apt --fix-broken install -y')
+os.system('sudo apt install -f -y')
+os.system('sudo apt autoremove -y')
 
-question = input("If you need to re-login or reboot? y/n:")
+question = input("If you need to re-login or reboot? Default is (N)y/n:")
 start_over = "action"
 if question == "y":
     start_over = os.system("telinit 6")
