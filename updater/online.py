1c97eb36e1fe7f42352da4351a480b6334a8afe3
nu11secur1ty <venvaropt@gmail.com>
2020-04-27 12:47:43 +0300

    Update online.py

--- updater/online.py
+++ updater/online.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import os
+import time
 os.system('sudo rm -rf *unofficial_up.py*')
+print ("Start : %s" % time.ctime())
+time.sleep( 5 )
 os.system('wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/updater/unofficial_up.py')
 os.system('python3 unofficial_up.py')
