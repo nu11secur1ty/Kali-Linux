ae91361d9b4e038a25dd3cacab502ba728dd35f0
nu11secur1ty <venvaropt@gmail.com>
2020-06-07 15:51:14 +0300

    Update zone.sh

--- 2020.x/datatime/zone.sh
+++ 2020.x/datatime/zone.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
 rm -rf *datter.sh*
-wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/2020.x/datatime/datter.sh 
-bash datter.sh
+wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/2020.x/datatime/datter.sh
