fd9180d8bf9715222b1f18658b74178eaf89aacc
nu11secur1ty <venvaropt@gmail.com>
2020-12-28 03:31:01 -0500

    Create local_update.py

---
+++ updater/local_update.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Author @nu11secur1ty
+import os
+os.system('sudo apt update -y')
+os.system('sudo apt install -f -y')
+os.system('sudo apt --fix-broken install -y')
+os.system('sudo apt --fix-missing install -y')
+os.system('sudo apt autoremove -y')
+# Dist upgrade
+os.system('sudo apt dist-upgrade -y')
+os.system('sudo apt -y full-upgrade -y')
+os.system('sudo dpkg --configure -a')
+os.system('sudo apt --fix-broken install -y')
+os.system('sudo apt install -f -y')
+os.system('sudo apt autoremove -y')
+
+# Offline mode
+question = input("If you need to re-login or reboot? Default is (N)y/n:")
+os.system('echo')
+if question == "y":
+    start_over = os.system("telinit 6")
+else:
+    raise SystemExit
