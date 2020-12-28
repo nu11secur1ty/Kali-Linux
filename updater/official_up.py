f03da06458da23f734d22f8b725cc98390be6122
nu11secur1ty <venvaropt@gmail.com>
2020-12-28 03:36:41 -0500

    Update official_up.py

--- updater/official_up.py
+++ updater/official_up.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 # Author @nu11secur1ty
 import os
+# Checking 
+print("Checking what is for upgrade\n")
+os.sysytem('apt list --upgradable')
+#
 os.system('sudo apt update -y')
+os.system('sudo apt upgrade -y')
 os.system('sudo apt install -f -y')
 os.system('sudo apt --fix-broken install -y')
 os.system('sudo apt --fix-missing install -y')
