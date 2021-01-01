835573460f1a9761f6b6445247722c33fcfce9c9
nu11secur1ty <venvaropt@gmail.com>
2021-01-01 20:02:15 +0200

    Update vpn.sh

--- VPN-Cisco/vpn.sh
+++ VPN-Cisco/vpn.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+apt purge libtss2-esys-3.0.2-0 libtss2-mu0 libtss2-sys1 -y
 apt install -y network-manager-openvpn-gnome
 apt install -y network-manager-vpnc-gnome
 apt install -y network-manager-openconnect-gnome
