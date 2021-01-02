00d86d05cda872cf58570778f942eab79241013c
nu11secur1ty <venvaropt@gmail.com>
2021-01-02 11:57:39 +0200

    Update local_update.py

--- updater/local_update.py
+++ updater/local_update.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 # Author @nu11secur1ty
 import os
-os.system('sudo apt update -y')
-os.system('sudo apt install -f -y')
-os.system('sudo apt --fix-broken install -y')
-os.system('sudo apt --fix-missing install -y')
-os.system('sudo apt autoremove -y')
+os.system('apt update -y')
+os.system('apt install -f -y')
+os.system('apt --fix-broken install -y')
+os.system('apt --fix-missing install -y')
+os.system('apt autoremove -y')
 # Dist upgrade
-os.system('sudo apt dist-upgrade -y')
-os.system('sudo apt -y full-upgrade -y')
-os.system('sudo dpkg --configure -a')
-os.system('sudo apt --fix-broken install -y')
-os.system('sudo apt install -f -y')
-os.system('sudo apt autoremove -y')
+os.system('apt dist-upgrade -y')
+os.system('apt -y full-upgrade -y')
+os.system('dpkg --configure -a')
+os.system('apt --fix-broken install -y')
+os.system('apt install -f -y')
+os.system('apt autoremove -y')
 
 # Offline mode
 question = input("If you need to re-login or reboot? Default is (N)y/n:")
