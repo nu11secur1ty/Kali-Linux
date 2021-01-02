c5c41fb48c269d681e899fc2c36a811efd3c08a7
nu11secur1ty <venvaropt@gmail.com>
2021-01-02 12:30:44 +0200

    Update omnium.pl

--- SUDO/omnium.pl
+++ SUDO/omnium.pl
@@ -8,7 +8,7 @@ print color('bold red');
 print "Protect root from sudo\n";
   print color('reset');
   
-my $omni = `sudo sed -i 's/root/#root/' /etc/sudoers`;
+my $omni_root = `sudo sed -i 's/root/#root/' /etc/sudoers`;
 my $omni_sudo = `sudo sed -i 's/%sudo/#%sudo/' /etc/sudoers`;
 print color('bold blue');
   print "Done;";
