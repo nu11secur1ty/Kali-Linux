8a5bcd80a8de6e353658f20009cb322a16d6652b
nu11secur1ty <venvaropt@gmail.com>
2021-01-02 11:10:20 +0200

    Update omnium.pl

--- SUDO/omnium.pl
+++ SUDO/omnium.pl
@@ -4,10 +4,6 @@ use warnings;
 use diagnostics;
 use Term::ANSIColor;
 
-print color('bold green');
-our $use_su = `sudo passwd root`;
-  print color('reset');
-
 print color('bold red');
 print "Protect root from sudo\n";
   print color('reset');
