3e9275382f91172998adc3e6f0619e8d108ae513
nu11secur1ty <venvaropt@gmail.com>
2021-08-28 23:07:19 -0700

    Add files via upload

---
+++ brave-browser-Kali-Linux/kukurus.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+apt install apt-transport-https -y
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ trusty main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-trusty.list
+apt update
+apt install brave-browser -y
+exit 0;
