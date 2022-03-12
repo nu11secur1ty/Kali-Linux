37eeaeaf37670f782bb5ccc60aaf093f6c651b45
nu11secur1ty <venvaropt@gmail.com>
2022-03-12 14:37:26 -0500

    Update unofficial_up.py

--- updater/unofficial_up.py
+++ updater/unofficial_up.py
@@ -5,6 +5,7 @@ os.system('apt update -y')
 os.system('apt install -f -y')
 os.system('apt --fix-broken install -y')
 os.system('apt --fix-missing install -y')
+os.system('apt full-upgrade -y')
 os.system('apt autoremove -y')
 # Dist upgrade
 os.system('apt dist-upgrade -y')
