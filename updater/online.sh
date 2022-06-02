bc14cdda1e6eb8bcee801ad23904262cf91fd39b
nu11secur1ty <venvaropt@gmail.com>
2022-06-02 11:50:52 +0300

    Update online.sh

--- updater/online.sh
+++ updater/online.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-sudo rm -rf *unofficial_up.py*
+sudo rm -rf *official_up.py*
   sleep 3
 wget https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/updater/official_up.py
 echo -e "\n";
 sudo python3 official_up.py
-sudo rm -rf *unofficial_up.py*
+sudo rm -rf *official_up.py*
   echo -e "\n";
   exit 0
