d3f503147c5de0737dbe85fdf48b1d2634278211
nu11secur1ty <venvaropt@gmail.com>
2022-06-02 12:27:23 +0300

    Update official_up.py

--- updater/official_up.py
+++ updater/official_up.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 # Author @nu11secur1ty
 import os
-# Checking 
-print("Checking what is for upgrade\n")
-os.system('apt list -a --upgradable')
-#
 os.system('apt update -y')
-os.system('apt upgrade -y')
-os.system('apt dist-upgrade -y')
 os.system('apt install -f -y')
-os.system('dpkg --configure -a')
 os.system('apt --fix-broken install -y')
 os.system('apt --fix-missing install -y')
+os.system('apt full-upgrade -y')
+os.system('apt autoremove -y')
+# Dist upgrade
+os.system('apt dist-upgrade -y')
+os.system('apt -y full-upgrade -y')
+os.system('dpkg --configure -a')
+os.system('apt --fix-broken install -y')
+os.system('apt install -f -y')
 os.system('apt autoremove -y')
