82a77445baa6cc9c4af8e47a54642ee4fa907904
nu11secur1ty <venvaropt@gmail.com>
2022-09-13 09:52:00 +0300

    Update official_up.py

--- updater/official_up.py
+++ updater/official_up.py
@@ -7,6 +7,7 @@ os.system('apt --fix-broken install -y')
 os.system('apt --fix-missing install -y')
 os.system('apt full-upgrade -y')
 os.system('apt autoremove -y')
+os.system('dpkg --configure -a')
 # Dist upgrade
 os.system('apt dist-upgrade -y')
 os.system('apt -y full-upgrade -y')
