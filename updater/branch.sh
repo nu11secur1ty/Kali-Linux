5662f360c6c906888aeb3ff99106cd7c8ebcb9c6
nu11secur1ty <venvaropt@gmail.com>
2022-12-26 23:15:54 +0200

    Update branch.sh

--- updater/branch.sh
+++ updater/branch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # Main Branch
 # kali-rolling
-echo "deb http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
+echo "deb http://kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
 # kali-last-snapshot
-echo "deb-src http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
+echo "deb-src http://kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
