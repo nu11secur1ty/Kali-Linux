7bdaed2a9b22a64cf53796273976c0aa47418c2c
nu11secur1ty <venvaropt@gmail.com>
2024-04-11 17:28:26 +0300

    Update downloader.sh

--- Soof-SMS/downloader.sh
+++ Soof-SMS/downloader.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 git clone --depth=1 https://github.com/nu11secur1ty/Kali-Linux.git
 cd Kali-Linux
-git sparse-checkout set SMSend-Anon-SMS-Sender
-git sparse-checkout set --no-cone SMSend-Anon-SMS-Sender
+git sparse-checkout set Soof-SMS
+git sparse-checkout set --no-cone Soof-SMS
 exit 0;
