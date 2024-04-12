62d2d0ff5b7febc3133e738c6db5d04f9ed14ff4
nu11secur1ty <venvaropt@gmail.com>
2024-04-12 13:14:57 +0300

    Update setup.sh

--- spoofinger/setup.sh
+++ spoofinger/setup.sh
@@ -1,1 +1,8 @@
-spoofinger
+#!/usr/bin/bash
+# spoofinger by nu11secur1ty
+git clone --depth=1 https://github.com/nu11secur1ty/Kali-Linux.git
+cd Kali-Linux
+git sparse-checkout set spoofinger
+git sparse-checkout set --no-cone spoofinger
+
+exit 0;
